144aead050bc2a8088752b01a9dffbd0c95e7a9a
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-01 17:24:58 +0545

    area

--- circle.c
+++ circle.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    double radius, diameter, circumference, area;
+     int radius;
+        float area;
+        printf("Enter the radius of the circle:");
+        scanf("%d", &radius);
+        area = 3.14 * radius * radius;
+        printf("Area of the circle = %.2f\n", area);
     return 0;
 }
