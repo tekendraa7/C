6f00681db9f586d5ae4c4b7ba430cf65617fa808
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-04 21:34:41 +0545

    commit

--- cylinder.c
+++ cylinder.c
@@ -7,6 +7,6 @@ printf("Enter the radius of the cylinder:");
 scanf("%d", &radius);
 printf("Enter the height of the cylinder:");
 scanf("%d", &height);
-printf("Volume of the cylinder = %d\n", 3.14 * radius * radius * height);
+printf("Volume of the cylinder = %f\n", 3.14 * radius * radius * height);
     return 0;
 }
