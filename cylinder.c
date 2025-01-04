758b65c12748d58d4f131e2555a962326e97d562
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-04 21:33:00 +0545

    volume of cylinder

---
+++ cylinder.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+// to measure the volume of cylinder
+int radius, height;
+printf("Enter the radius of the cylinder:");
+scanf("%d", &radius);
+printf("Enter the height of the cylinder:");
+scanf("%d", &height);
+printf("Volume of the cylinder = %d\n", 3.14 * radius * radius * height);
+    return 0;
+}
