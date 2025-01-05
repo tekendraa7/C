e05c07aec5f1383e1a8cf5fc2f1209665ad320ab
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-05 12:43:34 +0545

    variables

---
+++ 02-chapter/variable.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i=10; // Declare and initialize i with 10 
+    int j= i; // Declare and initialize j with i
+    // %d is called format specifier 
+    // %d is for int, %f is for float, %c is for char, %s is for string
+    printf("The value of i is %d and value of j is %d\n", i, j);
+    return 0;
+}
