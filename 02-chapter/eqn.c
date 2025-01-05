a8703f14bcd45ef08a6efcb5b5260c97f9ad9367
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-05 13:05:40 +0545

    equation

---
+++ 02-chapter/eqn.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 2;
+    int y = 3;
+    int z = 3;
+    int k = 1;
+    float e = 3*x/y - z+k;
+    printf("The value of e is %f\n", e);
+    return 0;
+}
