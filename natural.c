0a22eb21b36f75016d794e7ebc4f5baa945118b2
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-19 21:43:07 +0545

    natural number

---
+++ natural.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0) {
+        printf("%d is a natural number.\n", num);
+    } else {
+        printf("%d is not a natural number.\n", num);
+    }
+
+    return 0;
+}
