da330a6994b695fd214c880e97c2c2bd00ac4069
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-20 23:45:47 -0700

    while loops

---
+++ 04-loops/while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int count = 0;
+
+    // Example of a while loop
+    while (count < 5) {
+        printf("Count is: %d\n", count);
+        count++;
+    }
+
+    printf("Loop finished. Final count is: %d\n", count);
+
+    return 0;
+}
