95a2559805bdec64bffc3317792e3b3da0f2dbb3
Tekendra Bhandari <tekendraa67@gmail.com>
2025-02-20 06:50:54 +0545

    loops

---
+++ 04-loops/ll.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int i, j;
+    for (i=1; i<=3; i++) {
+        for (j=1; j<=4; j++) {
+            printf("%d %d\n", i, j);
+        }
+    }
+    return 0;
+}
