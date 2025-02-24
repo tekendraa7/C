36630fe30549b5493783bffa9164d3227b329752
Tekendra Bhandari <tekendraa67@gmail.com>
2025-02-24 09:55:01 +0545

    dfdfh

---
+++ 03-chapter/quadratic.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c;
+    double discriminant, root1, root2, realPart, imaginaryPart ;
+
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {
+        // Two distinct and real roots
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("Roots are real and different.\n");
+        printf("Root 1 = %.2lf\n", root1);
+        printf("Root 2 = %.2lf\n", root2);
+    } else if (discriminant == 0) {
+        // Two equal and real roots
+        root1 = root2 = -b / (2 * a);
+        printf("Roots are real and the same.\n");
+        printf("Root 1 = Root 2 = %.2lf\n", root1);
+    } else {
+        // Complex roots
+        realPart = -b / (2 * a);
+        imaginaryPart = sqrt(-discriminant) / (2 * a);
+        printf("Roots are complex and different.\n");
+        printf("Root 1 = %.2lf + %.2lfi\n", realPart, imaginaryPart);
+        printf("Root 2 = %.2lf - %.2lfi\n", realPart, imaginaryPart);
+    }
+
+    return 0;
+}
