6cae02bfb803c1903038983f75efac4c28f55b56
Tekendra Bhandari <tekendraa67@gmail.com>
2025-03-11 10:24:09 +0545

    Summing Numbers Until Sentinel Value

---
+++ do_while/summing_sentinel.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+    printf("Enter numbers to sum. Enter -1 to stop.\n");
+
+    do {
+        printf("Enter a number: ");
+        scanf("%d", &num);
+        if (num != -1) {
+            sum += num;
+        }
+    } while (num != -1); // Stop when -1 is entered
+
+    printf("Total sum: %d\n", sum);
+    return 0;
+}
