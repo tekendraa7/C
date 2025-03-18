4dc268573ded95af9b41b1ba0610e2f0d48c89ae
Tekendra Bhandari <tekendraa67@gmail.com>
2025-03-18 20:40:47 +0545

    ws2s

---
+++ z-exams/condis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, result;
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    if ((num1 % 2 == 0 && num2 % 2 != 0) || (num1 % 2 != 0 && num2 % 2 == 0))
+        result = num1 + num2;
+    else
+        result = num1 * num2;
+
+    printf("Result: %d\n", result);
+    return 0;
+}
