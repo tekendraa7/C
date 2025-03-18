a3d98984aac132c915db7d0c959b0a18bd0fd13a
Tekendra Bhandari <tekendraa67@gmail.com>
2025-03-18 20:05:12 +0545

    hkh

--- 03-chapter/LCM.c
+++ 03-chapter/LCM.c
@@ -9,19 +9,14 @@ int gcd(int a, int b) {
     }
     return a;
 }
-
 // Function to calculate the Least Common Multiple (LCM)
 int lcm(int a, int b) {
     return (a * b) / gcd(a, b);
 }
-
 int main() {
     int num1, num2;
-
     printf("Enter two integers: ");
     scanf("%d %d", &num1, &num2);
-
     printf("LCM of %d and %d is %d\n", num1, num2, lcm(num1, num2));
-
     return 0;
 }
