ae5eb8dcde2b0185bc09cfe10fb2fc216bd2737a
Tekendra Bhandari <tekendraa67@gmail.com>
2025-03-31 13:42:38 +0545

    quadratic equition

--- z-exams/quadratic.c
+++ z-exams/quadratic.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
 #include<math.h>
 int main() {
-    double a, b, c, d, root1, root2;
+    float a, b, c, disc, root1, root2, real, img;
     printf("Enter coefficients a, b and c: ");
-    scanf("%lf %lf %lf", &a, &b, &c);
-    d = b * b - 4 * a * c;
-    if (d > 0) {
-        root1 = (-b + sqrt(d)/(2 * a));
-        root2 = (-b - sqrt(d)/(2 * a));
-        printf("Root1 = %.2lf and Root2 = %.2lf\n", root1, root2);
+    scanf("%f %f %f", &a, &b, &c);
+    disc = b * b - 4 * a * c;
+    if (disc == 0) {
+        printf("Roots are real and same\n");
+        root1 = root2 = -b / (2 * a);
+        printf("Root 1 = Root 2 = %f\n", root1);
+    }
+    else if (disc > 0) {
+        printf("Roots are real and different\n");
+        root1 = (-b + sqrt(disc)) / (2 * a);
+        root2 = (-b - sqrt(disc)) / (2 * a);
+        printf("Root 1 = %f Root 2 =%f \n", root1, root2);
     }
     else {
-        printf("Roots are imaginary\n");
+        printf("Roots are complex and different\n");
+        real = -b / (2 * a);
+        img = sqrt(-disc) / (2 * a);
+        printf("Root 1 = %f + %fi\n", real, img);
+        printf("Root 2 = %f - %fi\n", real, img);
     }
     return 0;
 }
