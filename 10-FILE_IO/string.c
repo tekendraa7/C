96159bc2fd797ac7ba44bc5699a2c6a73a086f3d
Tekendra Bhandari <tekendraa67@gmail.com>
2025-04-08 17:30:06 +0545

    string

---
+++ 10-FILE_IO/string.c
@@ -0,0 +1,18 @@
+//Reading from file using fgets() function
+#include <stdio.h>
+#include <string.h>
+int main() {
+    FILE *fp;
+    char str[100];
+    fp = fopen("file.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    // Read a line from the file
+    while (fgets(str, 3, fp) != NULL) {
+        printf("%s", str);
+    }
+    fclose(fp);
+    return 0;
+}
